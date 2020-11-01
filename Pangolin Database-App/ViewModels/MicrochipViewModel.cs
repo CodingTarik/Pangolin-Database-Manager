bea4f81ea1d050c04c8a7161c20efb3647b89bef
CodingTarik <32739295+CodingTarik@users.noreply.github.com>
2020-11-01 17:36:20 +0100

    added microchip view

--- Pangolin Database-App/ViewModels/MicrochipViewModel.cs
+++ Pangolin Database-App/ViewModels/MicrochipViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Pangolin_Database_App.Database;
 using Pangolin_Database_App.Models;
+using System;
 
 namespace Pangolin_Database_App.ViewModels
 {
@@ -7,7 +8,13 @@ namespace Pangolin_Database_App.ViewModels
     {
         public MicrochipViewModel(Pangolin p = null) : base(DatabaseManager.GetDatabase().Microchips)
         {
-            this.SelectedModel = new Microchip { ReferenceNumber = p };
+            this.SelectedModel = new Microchip { ReferenceNumber = p, Date = DateTime.Now };
+            this.UpdateModelEvent += MicrochipViewModel_UpdateModelEvent;
+        }
+
+        private void MicrochipViewModel_UpdateModelEvent(object sender, EventArgs e)
+        {
+            this.SelectedModel = new Microchip { ReferenceNumber = SelectedPangolin, Date = DateTime.Now };
         }
     }
 }
